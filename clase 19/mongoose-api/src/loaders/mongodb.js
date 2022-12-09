3e999216b663408b47cbbfbfa48bcecefbf77e24
suarezmariano <marianosuarez@hotmail.com>
2022-12-09 17:47:26 -0300

    subida de cambios

--- clase 19/mongoose-api/src/loaders/mongodb.js
+++ clase 19/mongoose-api/src/loaders/mongodb.js
@@ -1,7 +1,7 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 async function startDB() {
-    await mongoose.connect('mongodb://localhost/coder')
+  await mongoose.connect('mongodb://127.0.0.1/coderhouse');
 }
 
-module.exports = startDB
+module.exports = startDB;
